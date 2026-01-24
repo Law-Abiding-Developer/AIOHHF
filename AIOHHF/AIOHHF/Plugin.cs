c9e35c16fd48cf435fdef343fac4a4b3627592e7
Law Abiding Coder <colton.trash5.5@proton.me>
2026-01-23 23:51:51 -0600

    Fixed major save bug

--- AIOHHF/AIOHHF/Plugin.cs
+++ AIOHHF/AIOHHF/Plugin.cs
@@ -32,23 +32,12 @@ public class Plugin : BaseUnityPlugin
         Harmony.CreateAndPatchAll(Assembly, $"{PluginInfo.PLUGIN_GUID}");
         Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_NAME} is loaded!");
         WaitScreenHandler.RegisterLateAsyncLoadTask(PluginInfo.PLUGIN_NAME, Aiohhf.RegisterPrefab, "Loading All-In-One Hand Held Fabricator");
-        WaitScreenHandler.RegisterAsyncLoadTask(PluginInfo.PLUGIN_NAME, LoadAssetBundle, "Loading FragmentsTechType for the All-In-One Hand Held Fabricator");
         Preinitialize();
-    }
-
-    public static IEnumerator LoadAssetBundle(WaitScreenHandler.WaitScreenTask task)
-    {
-        var assetBundleRequest = AssetBundle.LoadFromFileAsync(Path.Combine(Path.GetDirectoryName(Assembly.Location), "Assets", "aiohhfbundle"));
-        while (!assetBundleRequest.isDone)
-        {
-            task.Status = $"Loading assets... {assetBundleRequest.progress}";
-            yield return null;
-        }
-        Aiohhf.Bundle = assetBundleRequest.assetBundle;
     }
 
     public static void Preinitialize()
     {
+        Aiohhf.Bundle = AssetBundle.LoadFromFile(Path.Combine(Path.GetDirectoryName(Assembly.Location), "Assets", "aiohhfbundle"));
         Aiohhf.PrefabInfo = PrefabInfo.WithTechType("AIOHHF", "All-In-One Hand Held Fabricator", 
                 "An All-In-One Hand Held Fabricator (AIOHHF). This fabricator has all other Fabricators! And is Hand Held(tm)!" +
                 "\nUnfortunately, it holds no data of the Fabricator, you'll have to give it data. " +
